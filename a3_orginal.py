e00f9cd5d4497d62f0c853b4d390db5d1db4bfb9
Code Help Tutor <1823890830@qq.com>
2024-03-26 03:28:50 +0000

    WeChat cstutorcs Extra Fancy Sokoban A3

---
+++ a3_orginal.py
@@ -0,0 +1,20 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+import tkinter as tk
+from tkinter import messagebox, filedialog
+from typing import Callable
+from model import SokobanModel, Tile, Entity,convert_maze,Goal,Coin,Crate,Potion,Wall
+from a2_support import *
+from a3_support import *
+# Write your classes and functions here
+
+
+def main():
+    """ The main function. """
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
